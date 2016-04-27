775295309851896c70459d2be9ffb657e01005d6
zhang1995 <zhangtong@utexas.edu>
2016-04-27 12:58:43 -0500

    start working on the GUI

--- src/mastermind/GameDriver.java
+++ src/mastermind/GameDriver.java
@@ -2,85 +2,89 @@ package mastermind;
 
 import java.util.Scanner;
 
-public class GameDriver {
-	String[] hello = { "Hello", "world" };
+import javax.swing.*;
+
+public class GameDriver extends JFrame {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
 		// begin the game?
-		Ready_Play();
+
+		int start = Ready_Play();
 
 		// the computer will generate a random codes
 		Codes code = new Codes();
-		
-		for(String c : code.codes) {
-			System.out.println(c);
-		}
-		// ask the user to input guesses
-		int guess_left = 12;
-		String guess_code;
-		while (guess_left > 0) {
-			guess_code = Input_Guess(guess_left);
-			
-			if(ParseGuess(guess_code)) {
-				Pegs_Result peg = new Pegs_Result();
-				peg.Set_Result(code, guess_code);
-				System.out.printf("White peg: %d\nBlack peg: %d\n", peg.white, peg.black);
-				guess_left -= 1;
-			}else {
-				System.out.println("INVALID GUESS");
-			}
-			
-			
-			
-		}
+		while (start == JOptionPane.YES_OPTION) {
+			//creates a GUI for the game using swing? or applet?
+			new MasterMind_GUI().setVisible(true);
+			// ask the user to input guesses
+			int guess_left = 12;
+			String guess_code;
+			while (guess_left > 0) {
+				guess_code = Input_Guess(guess_left);
 
+				if (ParseGuess(guess_code)) {
+					Pegs_Result peg = new Pegs_Result();
+					peg.Set_Result(code, guess_code);
+					System.out.printf("White peg: %d\nBlack peg: %d\n", peg.white, peg.black);
+					guess_left -= 1;
+				} else {
+					System.out.println("INVALID GUESS");
+				}
+
+			}
+		}
 	}
 
 	/*
 	 * asked if the user is ready
 	 * 
 	 */
-	public static void Ready_Play() {
-		Scanner in = new Scanner(System.in);
-		String res;
-		System.out.println("You have 12 guesses to figure out the secret"
-				+ " code or you lose the game. Are you ready to play? (Y/N):");
+	public static int Ready_Play() {
+		/*
+		 * Scanner in = new Scanner(System.in); String res; System.out.println(
+		 * "You have 12 guesses to figure out the secret" +
+		 * " code or you lose the game. Are you ready to play? (Y/N):");
+		 * 
+		 * res = in.nextLine();
+		 */
 
-		res = in.nextLine();
+		return JOptionPane
+				.showConfirmDialog(null,
+						"You have 12 guesses to figure out the secret"
+								+ " code or you lose the game. Are you ready to play? (Y/N):",
+						null, JOptionPane.YES_NO_OPTION);
 
 	}
+
 	/*
 	 * promt the user to enter a guess
 	 * 
 	 */
 	public static String Input_Guess(int guess_left) {
 		Scanner in = new Scanner(System.in);
-		System.out.printf("You have %d guesses left.\n"
-				+ "What is your next guess?\n"
-				+ "Type in the characters for your guess and press enter. \n"
-				+ "Enter guess: ", guess_left);
-		
+		System.out.printf("You have %d guesses left.\n" + "What is your next guess?\n"
+				+ "Type in the characters for your guess and press enter. \n" + "Enter guess: ", guess_left);
+
 		return in.nextLine();
-		
+
 	}
-	
+
 	/*
 	 * determine if the guess is valid
 	 * 
 	 */
 	public static boolean ParseGuess(String guess) {
-		
-		if(guess.length() == 4) {
+
+		if (guess.length() == 4) {
 			boolean valid = guess.matches("[RBGPOY]*");
-			if(valid){
+			if (valid) {
 				return true;
-			}	
+			}
 		}
-		
+
 		return false;
-		
+
 	}
 
 }
---
+++ src/mastermind/MasterMind_GUI.java
@@ -0,0 +1,24 @@
+package mastermind;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JFrame;
+
+public class MasterMind_GUI extends JFrame implements ActionListener{
+
+	public MasterMind_GUI() {
+		super("MasterMind");
+		setSize(300, 500);
+		setResizable(false);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+}

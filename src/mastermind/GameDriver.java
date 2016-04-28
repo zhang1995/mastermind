95953accb1aa02ae5794093cf59f776b25b55280
Kaisheng Song <jason.song0902@gmail.com>
2016-04-28 13:10:04 +0800

    no message

--- src/mastermind/GameDriver.java
+++ src/mastermind/GameDriver.java
@@ -40,9 +40,20 @@ public class GameDriver {
 	}
 	
 	public static void Ready_Play(){
+		System.out.println("Welcome to Mastermind.  Here are the rules.\n");
+		System.out.println("This is a text version of the classic board game Mastermind.\n"
+				+ "The computer will think of a secret code. The code consists of 4 colored pegs. The pegs may be\n"
+				+ "one of six colors: blue, green, orange, purple, red, or yellow. \n"
+				+ "A color may appear more than once in the code. You try to guess what colored pegs are in the code and\n"
+				+ "what order they are in.   After you make a valid guess the result (feedback) will be displayed.\n"
+				+ "The result consists of a black peg for each peg you have guessed exactly correct (color and position)\n"
+				+ "in your guess.  For each peg in the guess that is the correct color, but is out of position, you get a\n"
+				+ "white peg. For each peg, which is fully incorrect, you get no feedback.\n\n"
+				+ "Only the first letter of the color is displayed. B for Blue, R for Red, and so forth.\n"
+				+ "When entering guesses you only need to enter the first character of each color as a capital letter.\n");
 		Scanner in = new Scanner(System.in);
 		String res;
-		System.out.println("You have 12 guesses to figure out the secret code or you lose the game.  Are you ready to play? (Y/N):");
+		System.out.print("You have 12 guesses to figure out the secret code or you lose the game.  Are you ready to play? (Y/N):");
 		res = in.nextLine();
 		while(!res.equals("Y") && !res.equals("N")){
 			System.out.println("Wrong input, again!");
